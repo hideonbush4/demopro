410427ad99312d3a2a84a719e3fc5004eb0ff972
zhewen.deng <zhewen.deng@macrowing.com>
2022-03-03 09:27:06 +0800

    basePackages

--- src/main/java/com/example/demo/dao/UserMapper.java
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -2,7 +2,9 @@ package com.example.demo.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.demo.domain.entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 
 }
